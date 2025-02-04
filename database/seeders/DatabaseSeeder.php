ba1f4d5ee92d541847afeed9215a32f066da1470
Darinel <darinelcigarroa97@gmail.com>
2025-02-04 17:04:42 -0600

    add userSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,8 @@ class DatabaseSeeder extends Seeder
 
         User::factory()->create([
             'name' => 'Test User',
-            'email' => 'test@example.com',
+            'email' => 'user@user.com',
+            'password' => 'password',
         ]);
     }
 }
