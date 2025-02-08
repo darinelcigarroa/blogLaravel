5a1d50035cda77162694a0edf58d47bb43c5b7cf
Darinel <darinelcigarroa97@gmail.com>
2025-02-08 17:54:57 -0600

    add tailwind styles

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -1,22 +1,22 @@
 <x-dashboard>
-    <h1>Listado de los blogs:</h1>
+    <p class="text-4xl">Listado de los blogs:</p>
     <br>
     @session('status')
         <div>{{ $value }}</div>
     @endsession
-    <div style="background: green; width: 200px; padding: 5px; border-radius: 20px; display: flex; justify-content:center; align-items: center">
-        <a href="{{ route('blog.create') }}">Crear nuevo post</a>
+    <div class="max-w-max p-4 rounded-xl cursor-pointer bg-blue-500">
+        <a class="w-xs" href="{{ route('blog.create') }}">Crear nuevo post</a>
     </div>
     <ul>
         @foreach ($blogs as $blog)
-            <div style="display: flex; align-items: baseline; margin:20px;">
+            <div class="flex mt-2 p-2 items-center">
                 <li><a href="{{ route('blog.show', $blog) }}">{{ $blog->title }}</a></li>
-                <div style="margin-left: 10px; display: flex" >
-                    <a style="background: rgb(0, 191, 255); border-radius: 15px; padding: 8px" href="{{ route('blog.edit', $blog )}}">Editar</a>
+                <div class="flex" >
+                    <a class="ml-3 bg-cyan-600 rounded-xl p-2" href="{{ route('blog.edit', $blog )}}">Editar</a>
                     <form action="{{ route('blog.destroy', $blog) }}" method="POST">
                         @method('DELETE')
                         @csrf
-                        <input style="cursor:pointer; margin-left: 8px; background: red; border-radius:15px; padding: 8px" type="submit" value="Eliminar">
+                        <input class="ml-3 bg-red-600 rounded-xl p-2" type="submit" value="Eliminar">
                     </form>
                 </div>
             </div>
