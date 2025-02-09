7103ab32ef58551c97ffa475cda826ef7f5d61d6
Darinel <darinelcigarroa97@gmail.com>
2025-02-08 18:03:47 -0600

    styles view blog show

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -1,11 +1,22 @@
 <x-dashboard>
-    <h1>Show post</h1>
-    <h4>Titulo:</h4>
-    {{ $blog->title }}
-    <h4>Descripcion:</h4>
-    {{ $blog->description }}
-    <h4>Cuerpo:</h4>
-    {{ $blog->body }}
-    <br>
-    <a href="{{ url()->previous() }}">Regresar</a>
+    <h1 class="text-5xl">Show post</h1>
+    
+    <h3 class="text-2xl mt-5">Titulo:</h3>
+    <span class="text-gray-400"> 
+        {{ $blog->title }}
+    </span>
+
+    <h3 class="text-2xl mt-5">Descripcion:</h3>
+    <span class="text-gray-400">
+        {{ $blog->description }}
+    </span>
+
+    <h3 class="text-2xl mt-5">Cuerpo:</h3>
+    <span class="text-gray-400">
+        {{ $blog->body }}
+    </span>
+
+    <div class="bg-cyan-700 w-max p-3 rounded-xl mt-5">
+        <a href="{{ url()->previous() }}">Regresar al listado de post</a>
+    </div>
 </x-dashboard>
